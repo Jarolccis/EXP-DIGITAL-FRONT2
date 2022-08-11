93c2463f04b56e8a631af53157852501b7c7a474
Jarol Cachay <ext_jcachayc@tottus.com.pe>
2022-08-11 16:20:52 -0500

    Auto-generated commit [ci skip]

---
+++ 834.84d4ea44764a6f00.js
@@ -0,0 +1,55 @@
+"use strict";
+(self["webpackChunkzxing_scanner_demo"] = self["webpackChunkzxing_scanner_demo"] || []).push([[834],{
+
+/***/ 4834:
+/*!*************************************************************************************************!*\
+  !*** ./projects/zxing-scanner-demo/src/app/modules/product-location/product-location.module.ts ***!
+  \*************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "ProductLocationModule": () => (/* binding */ ProductLocationModule)
+/* harmony export */ });
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/common */ 6895);
+/* harmony import */ var _components_search_search_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/search/search.component */ 136);
+/* harmony import */ var _pages_search_product_search_product_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./pages/search-product/search-product.component */ 8926);
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/forms */ 4006);
+/* harmony import */ var ngx_spinner__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ngx-spinner */ 8480);
+/* harmony import */ var ngx_toastr__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ngx-toastr */ 7185);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ 4650);
+
+
+
+
+
+
+
+
+
+let ProductLocationModule = /*#__PURE__*/(() => {
+  class ProductLocationModule {}
+
+  ProductLocationModule.ɵfac = function ProductLocationModule_Factory(t) {
+    return new (t || ProductLocationModule)();
+  };
+
+  ProductLocationModule.ɵmod = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineNgModule"]({
+    type: ProductLocationModule
+  });
+  ProductLocationModule.ɵinj = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineInjector"]({
+    imports: [_angular_common__WEBPACK_IMPORTED_MODULE_3__.CommonModule, _angular_forms__WEBPACK_IMPORTED_MODULE_4__.ReactiveFormsModule, _angular_forms__WEBPACK_IMPORTED_MODULE_4__.FormsModule, ngx_spinner__WEBPACK_IMPORTED_MODULE_5__.NgxSpinnerModule, ngx_toastr__WEBPACK_IMPORTED_MODULE_6__.ToastrModule.forRoot()]
+  });
+  return ProductLocationModule;
+})();
+
+(function () {
+  (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵsetNgModuleScope"](ProductLocationModule, {
+    declarations: [_components_search_search_component__WEBPACK_IMPORTED_MODULE_0__.SearchComponent, _pages_search_product_search_product_component__WEBPACK_IMPORTED_MODULE_1__.SearchProductComponent],
+    imports: [_angular_common__WEBPACK_IMPORTED_MODULE_3__.CommonModule, _angular_forms__WEBPACK_IMPORTED_MODULE_4__.ReactiveFormsModule, _angular_forms__WEBPACK_IMPORTED_MODULE_4__.FormsModule, ngx_spinner__WEBPACK_IMPORTED_MODULE_5__.NgxSpinnerModule, ngx_toastr__WEBPACK_IMPORTED_MODULE_6__.ToastrModule]
+  });
+})();
+
+/***/ })
+
+}]);
